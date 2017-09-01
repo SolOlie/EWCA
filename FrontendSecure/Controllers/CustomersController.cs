db2d3b00eeffa9652070d01f639f8e21ae9603d2
agmam <ag-mam@live.dk>
2017-09-01 09:09:19 +0200

    deleted methds in customer controller

--- FrontendSecure/Controllers/CustomersController.cs
+++ FrontendSecure/Controllers/CustomersController.cs
@@ -375,44 +375,6 @@ namespace FrontendSecure.Controllers
         }
 
         [HttpPost, ValidateInput(false)]
-        public ActionResult CustomerTableExpressPartialAddNew([ModelBinder(typeof(DevExpressEditorsBinder))] Entities.Entities.Customer item)
-        {
-            var model = new object[0];
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    // Insert here a code to insert the new item in your model
-                }
-                catch (Exception e)
-                {
-                    ViewData["EditError"] = e.Message;
-                }
-            }
-            else
-                ViewData["EditError"] = "Please, correct all errors.";
-            return PartialView("_CustomerTableExpressPartial", model);
-        }
-        [HttpPost, ValidateInput(false)]
-        public ActionResult CustomerTableExpressPartialUpdate([ModelBinder(typeof(DevExpressEditorsBinder))] Entities.Entities.Customer item)
-        {
-            var model = new object[0];
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    // Insert here a code to update the item in your model
-                }
-                catch (Exception e)
-                {
-                    ViewData["EditError"] = e.Message;
-                }
-            }
-            else
-                ViewData["EditError"] = "Please, correct all errors.";
-            return PartialView("_CustomerTableExpressPartial", model);
-        }
-        [HttpPost, ValidateInput(false)]
         public ActionResult CustomerTableExpressPartialDelete(string Id)
         {
 
