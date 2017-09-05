dbd9170dbc5cfc14c4c669a60dadb3c964744c32
Patrick Maagaard Hjorth Laursen <patrick.laursen@gmail.com>
2017-09-05 12:44:14 +0200

    No more password in assetTablePartical

--- FrontendSecure/Models/AssetListPartialModel.cs
+++ FrontendSecure/Models/AssetListPartialModel.cs
@@ -10,6 +10,5 @@ namespace FrontendSecure.Models
     {
         public List<Asset> Assets { get; set; }
         public int CustomerId { get; set; }
-        public string AssetPassword { get; set; }
     }
 }
