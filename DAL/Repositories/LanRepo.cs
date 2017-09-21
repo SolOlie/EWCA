d76aeeb614622d33486937ab2fa99cdc12ad5bc0
agmam <ag-mam@live.dk>
2017-09-21 13:17:11 +0200

    repo, controller and so on is made

---
+++ DAL/Repositories/LanRepo.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.DB;
+using Entities.Entities;
+
+namespace DAL.Repositories
+{
+    class LanRepo : IRepository<Lan>
+    {
+        public Lan Create(Lan t)
+        {
+            using (var ctx = new CADBContext())
+            {
+                ctx.Entry(t.Customer).State = EntityState.Unchanged;
+                Lan a = ctx.Lans.Add(t);
+
+                ctx.SaveChanges();
+                return a;
+            }
+        }
+
+        public Lan Read(int id)
+        {
+            using (var ctx = new CADBContext())
+            {
+                return ctx.Lans.Include(m => m.Customer).FirstOrDefault(x => x.Id == id);
+            }
+        }
+
+        public List<Lan> ReadAll()
+        {
+            using (var ctx = new CADBContext())
+            {
+                var c = ctx.Lans.Include(m => m.Customer).ToList();
+                return c;
+            }
+        }
+
+        public bool Delete(Lan t)
+        {
+            using (var ctx = new CADBContext())
+            {
+                if (LanExists(t.Id, ctx))
+                {
+                    ctx.Lans.Attach(t);
+                    ctx.Lans.Remove(t);
+                    ctx.SaveChanges();
+                    return true;
+                }
+                return false;
+
+
+            }
+        }
+
+        private bool LanExists(int tId, CADBContext ctx)
+        {
+            if (ctx.Lans.Count(a => a.Id == tId) > 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public bool Update(Lan t)
+        {
+            using (var ctx = new CADBContext())
+            {
+                ctx.Lans.Attach(t);
+                ctx.Entry(t).State = EntityState.Modified;
+                try
+                {
+                    ctx.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LanExists(t.Id, ctx))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return true;
+            }
+        }
+
+        public List<Lan> ReadAllWithFk(int id)
+        {
+            using (var ctx = new CADBContext())
+            {
+                var c = ctx.Lans.Include(m => m.Customer).Where(x => x.Customer.Id == id).ToList();
+                return c;
+            }
+        }
+    }
+}
---
+++ EWCustomerAccountingBackend/Controllers/FirewallsController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DAL;
+using DAL.DB;
+using DAL.Repositories;
+using Entities.Entities;
+
+namespace EWCustomerAccountingBackend.Controllers
+{
+    public class FirewallsController : ApiController
+    {
+        private IRepository<Firewall> db = new Facade().GetFirewallRepo();
+
+        // GET: api/Firewalls
+        public List<Firewall> GetFirewalls()
+        {
+            return db.ReadAll();
+        }
+
+        public List<Firewall> GetFirewallsWithFk(int id)
+        {
+            return db.ReadAllWithFk(id);
+        }
+
+        // GET: api/Firewalls/5
+        [ResponseType(typeof(Firewall))]
+        public IHttpActionResult GetFirewall(int id)
+        {
+            Firewall firewall = db.Read(id);
+            if (firewall == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(firewall);
+        }
+
+        // PUT: api/Firewalls/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutFirewall(int id, Firewall firewall)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != firewall.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Update(firewall);
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Firewalls
+        [ResponseType(typeof(Firewall))]
+        public IHttpActionResult PostFirewall(Firewall firewall)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Create(firewall);
+
+            return CreatedAtRoute("DefaultApi", new { id = firewall.Id }, firewall);
+        }
+
+        // DELETE: api/Firewalls/5
+        [ResponseType(typeof(Firewall))]
+        public IHttpActionResult DeleteFirewall(int id)
+        {
+            Firewall firewall = db.Read(id);
+            if (firewall == null)
+            {
+                return NotFound();
+            }
+
+            db.Delete(firewall);
+
+            return Ok(firewall);
+        }
+
+    }
+}
