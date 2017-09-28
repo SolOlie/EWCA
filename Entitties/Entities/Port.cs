518bf6dfa2aebde66a5831ef404da5f9855fbe12
Patrick Maagaard Hjorth Laursen <patrick.laursen@gmail.com>
2017-09-28 12:01:51 +0200

    Danish

--- Entitties/Entities/Port.cs
+++ Entitties/Entities/Port.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,10 @@ namespace Entities.Entities
         public int Id { get; set; }
         public int SwitchId { get; set; }
         public Switch Switch { get; set; }
+        [Display(Name = "Port nr.")]
         public int PortNumber { get; set; }
         public int AssetId { get; set; }
-        [DisplayName("Asset")]
+        [DisplayName("Udstyr")]
         public Asset Asset { get; set; }
         public string Trunk { get; set; }
         public string VLAN { get; set; }
